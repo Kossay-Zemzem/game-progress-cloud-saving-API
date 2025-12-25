48f71602e1f145185b308061b0a08435c0ec3c2a
Kossay-Zemzem <kossayzemzem@gmail.com>
2025-12-25 22:43:00 +0100

    feat: initialize Spring boot project with Spring initializer.

---
+++ src/main/java/com/devops/profileapi/ProfileapiApplication.java
@@ -0,0 +1,13 @@
+package com.devops.profileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileapiApplication.class, args);
+	}
+
+}
