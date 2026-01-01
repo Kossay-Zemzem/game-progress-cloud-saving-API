06edac8413e8b60c3010caba236d5027bb3c5ef7
Kossay-Zemzem <kossayzemzem@gmail.com>
2026-01-01 22:40:31 +0100

    feat: add profile saving endpoint

--- src/main/java/com/devops/profileapi/ProfileController.java
+++ src/main/java/com/devops/profileapi/ProfileController.java
@@ -1,9 +1,8 @@
 package com.devops.profileapi;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @Slf4j
@@ -16,13 +15,20 @@ public class ProfileController {
 
     @GetMapping("/profiles")
     public List<Profile> getAllProfiles() {
-        log.info("method=POST path=/profiles desc=Fetching all profiles");
+        log.info("method=GET path=/profiles desc=Fetching all profiles");
         return profileRepository.findAll();
     }
 
     @GetMapping("/profiles/{id}")
     public Profile getProfileById(@PathVariable("id") Long id) {
-        log.info("method=POST path=/profiles/{} desc=Fetching profile by id", id);
+        log.info("method=GET path=/profiles/{} desc=Fetching profile by id", id);
         return profileRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/profiles")
+    public ResponseEntity<Profile> createProfile(@RequestBody Profile profile) {
+        log.info("method=POST path=/profiles desc=Creating new profile {}", profile);
+        Profile savedProfile = profileRepository.save(profile);
+        return ResponseEntity.ok(savedProfile);
     }
 }
