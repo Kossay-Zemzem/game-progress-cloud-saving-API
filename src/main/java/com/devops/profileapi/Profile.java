eb6152a8bd2786c65a4c15379e654237d027324f
Kossay-Zemzem <kossayzemzem@gmail.com>
2026-01-01 22:41:00 +0100

    chore: delegate id handling to database sequence

--- src/main/java/com/devops/profileapi/Profile.java
+++ src/main/java/com/devops/profileapi/Profile.java
@@ -1,6 +1,8 @@
 package com.devops.profileapi;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -12,6 +14,7 @@ import lombok.*;
 @ToString
 public class Profile {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String profileName;
 
