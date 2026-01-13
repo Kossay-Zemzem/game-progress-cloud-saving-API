5c24ae606b49311932fdcd4e3a2f13b1f07268a6
Kossay-Zemzem <kossayzemzem@gmail.com>
2026-01-13 12:35:59 +0100

    feat: expose metrics for profile operations and add Prometheus support.

--- src/main/java/com/devops/profileapi/ProfileController.java
+++ src/main/java/com/devops/profileapi/ProfileController.java
@@ -1,5 +1,8 @@
 package com.devops.profileapi;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +15,40 @@ import java.util.List;
 @RestController
 public class ProfileController {
     ProfileRepository profileRepository;
-    ProfileController(ProfileRepository profileRepository) {
+    //Metrics
+    private final Counter profileCreateCounter;
+    private final Counter profileFetchCounter;
+    private final Timer profileFetchTimer;
+
+
+    ProfileController(ProfileRepository profileRepository, MeterRegistry meterRegistry) {
         this.profileRepository = profileRepository;
+
+        this.profileCreateCounter = Counter.builder("profiles.create.request")
+                .description("Total number of profiles creation requests")
+                .register(meterRegistry);
+        this.profileFetchCounter = Counter.builder("profiles.fetch.total")
+                .description("Total number of profile fetch requests")
+                .register(meterRegistry);
+        this.profileFetchTimer = Timer.builder("profiles.fetch.timer")
+                .description("Time taken to fetch profiles")
+                .register(meterRegistry);
     }
 
     @GetMapping("/profiles")
     public List<Profile> getAllProfiles() {
         log.info("method=GET path=/profiles desc=Fetching all profiles");
-        return profileRepository.findAll();
+        return profileFetchTimer.record(() -> {
+                profileRepository.findAll();
+                profileFetchCounter.increment(); //metric increment
+                return profileRepository.findAll();
+        });
     }
 
     @GetMapping("/profiles/{id}")
     public Profile getProfileById(@PathVariable("id") Long id) {
         log.info("method=GET path=/profiles/{} desc=Fetching profile by id", id);
+        profileFetchCounter.increment(); //metric increment
         return profileRepository.findById(id).orElse(null);
     }
 
@@ -32,6 +56,7 @@ public class ProfileController {
     public ResponseEntity<Profile> createProfile(@RequestBody Profile profile) {
         log.info("method=POST path=/profiles desc=Creating new profile, id={}", profile.getId());
         Profile savedProfile = profileRepository.save(profile);
+        profileCreateCounter.increment(); //metric increment
         URI location = URI.create(String.format("/profiles/%s", savedProfile.getId())); //return the location of the created resource as per REST conventions
         return ResponseEntity.created(location).body(savedProfile); //returns 201 created as per change request
     }
